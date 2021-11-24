076b5ad5e802c234404ccfdbdc11f474d210c0dd
s183802 <89857759+s183802@users.noreply.github.com>
2021-11-24 14:29:49 +0100

    Delete advection directory

--- advection/feltor.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-: ${FELTOR_PATH:="../feltor"}
-# If feltor is not here then change the FELTOR_PATH enviromnent variable
-# export FELTOR_PATH="path/to/feltor"
-
-make -C $FELTOR_PATH/src/navier_stokes/ navier_stokes
-
-$FELTOR_PATH/src/navier_stokes/navier_stokes $@
